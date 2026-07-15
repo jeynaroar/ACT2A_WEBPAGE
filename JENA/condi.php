f395d07370c7dc78a1ed28215a66de1fdd91cbeb
jeynaroar <antoniojena77@gmail.com>
2026-07-15 12:17:26 +0800

    Add files via upload

---
+++ JENA/condi.php
@@ -0,0 +1,26 @@
+<?php
+$score = 75;
+$att = 75;
+
+
+if ($score >= 80) {
+    if ($att >= 85) {
+        echo "Passed with honors";
+    } 
+    
+    else {
+       echo"Passed but needs to improve attendance";
+    }
+    
+}
+else {
+    
+    echo "Failed";
+}
+
+
+
+
+
+
+?>
---
+++ JENA/exercise.php
@@ -0,0 +1,26 @@
+<?php
+ 
+    $one = 10;
+    $two = 20;
+    $three = 30;
+    $four = 40;
+
+
+$sum = $one + $two;
+$diff = $three - $two;
+$pro = $one * $four;
+$quo = $four / $two;
+
+echo "The sum $one and $two is $sum. <br>";
+echo " The Difference between $three and $two is $diff. <br>";
+echo "The Product of $one and $four is $pro. <br>";
+echo "The Quotient of $four by $two is $quo. <br>";
+
+$total = $sum + $diff + $pro + $quo;
+echo " The total of all the results is $total. <br>";
+
+$ave = $total / 4;
+echo " The average of all the results is $ave.";
+
+
+?>
